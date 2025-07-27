18bf50fc7d503b85b6155a7bba05e103111626e4
Michael Jonker <michael@openpoint.ie>
2025-07-27 18:02:59 +0100

    Split copyright credits

--- .scripts/util.sh
+++ .scripts/util.sh
@@ -3,7 +3,8 @@
 : '
  * MIT License
  *
- * Copyright (c) 2025 Michael Jonker and Zachary Kleiman
+ * Copyright (c) 2025 Michael Jonker
+ * Copyright (c) 2025 Zachary Kleiman
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
