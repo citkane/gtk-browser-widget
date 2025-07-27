c5d682e3bef1696ac1e38b51149439106a555522
Zachary Kleiman <kleimanpolitics@gmail.com>
2025-07-27 02:10:28 -0400

    update util.cmake

--- .cmake/util.cmake
+++ .cmake/util.cmake
@@ -7,40 +7,20 @@ set(BROWSERS MSWEBVIEW2 WEBKIT CHROMIUM)
 ## - Linux: WEBKIT
 ## - Mac: WEBKIT
 function (set_browser_definition)
-    set(count 0)
-    # Get a count of how many embedded browsers have been defined
-    foreach(browser IN LISTS BROWSERS)
-        if(DEFINED "${browser}")
-            math(EXPR count "${count} + 1")
-        endif()
-    endforeach()
-    # Only one embedded browser type is allowed, so throw error if more are defined.
-    if("${count}" GREATER 1)
-        string(JOIN "," csv_list ${BROWSERS})
-        message(FATAL_ERROR "More than one embbeded browser was defined. Define ONE of: [${csv_list}]")
-    endif()
-    # Proceed if only one embedded browser has been defined
-    if("${count}" GREATER 0)
-        # Throw fatal error if MSWEBVIEW2 is defined for a non Windows OS
-        if(MSWEBVIEW2 AND NOT WIN32)
+    if(BROWSER=="mswebview2" AND NOT WIN32)
             message(FATAL_ERROR "MSWEBVIEW2 is only available on the Windows operating system")
         endif()
-        # Compile the embedded browser type definition
-        if(MSWEBVIEW2)
-            target_compile_definitions(${PROJECT_NAME} PRIVATE GBW_MSWEBVIEW2=ON)
-        endif()
-        if(WEBKIT)
-            target_compile_definitions(${PROJECT_NAME} PRIVATE GBW_WEBKIT=ON)
-        endif()
-        if(CHROMIUM)
-            target_compile_definitions(${PROJECT_NAME} PRIVATE GBW_CHROMIUM=ON)
-        endif()
-    # Set the default embedded browser if none has been defined
-    elseif(WIN32)
+    # Compile the embedded browser type definition
+    if(BROWSER=="mswebview2")
         target_compile_definitions(${PROJECT_NAME} PRIVATE GBW_MSWEBVIEW2=ON)
-    else()
+    endif()
+    if(BROWSER=="webkit")
         target_compile_definitions(${PROJECT_NAME} PRIVATE GBW_WEBKIT=ON)
     endif()
+    if(BROWSER=="chromium")
+        target_compile_definitions(${PROJECT_NAME} PRIVATE GBW_CHROMIUM=ON)
+    endif()
+    # Set the default embedded browser if none has been defined
 endfunction()
 
 function (set_browser_linked_lib)
